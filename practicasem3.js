210c1c7979b8a0b3e18fdfe544d48e2b1cfee861
carmen1810 <84483934+carmen1810@users.noreply.github.com>
2021-06-05 10:13:26 -0500

    Create practicasem3.js

---
+++ practicasem3.js
@@ -0,0 +1,25 @@
+///EJERCICIO 1///
+function getUniqueArray(array){
+    var uniqueArray = [];
+    if (array.length > 0) {
+       uniqueArray[0] = array[0];
+    }
+    for(var i = 0; i < array.length; i++){
+        var isExist = false;
+        for(var j = 0; j < uniqueArray.length; j++){
+            if(array[i] == uniqueArray[j]){
+                isExist = true;
+                break;
+            }
+            else{
+                isExist = false;
+            }
+        }
+        if(isExist == false){
+            uniqueArray[uniqueArray.length] = array[i];
+        }
+    }
+    return uniqueArray;
+}
+let a = ["Hare", "Krishna", "Hare", "Krishna", "Krishna", "Krishna", "Hare", "Hare", ":-O"];
+console.log(getUniqueArray(a));
